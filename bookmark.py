dc63acbd46683c88b5c1e11ce5afbc57deba453c
Royos90 <racerxjpn@gmail.com>
2022-02-06 16:58:11 -0800

    added the tweepy function which gets a user's likes

--- bookmark.py
+++ bookmark.py
@@ -1,7 +1,8 @@
 import tweepy
 import json 
+import pprint
 
-
+pp = pprint.PrettyPrinter(indent=20)
 #api keys 
 api_key = ""
 api_secrets = ""
@@ -22,6 +23,9 @@ except:
 
 
 user = api.get_user(screen_name='')
+twitterJsonResponse = api.get_favorites(id=,max_results=4)
+#for jsonObject in listOutputOfRequest:
+	#pp.pprint(jsonObject)
 print(user.name)
 print(user.description)
 print(user.location)
